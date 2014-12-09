bd9077dc6133894d33448783d0dd2c8b26bab36d
ebaibourine <ebaibourine@FC-Dell1414.XPX.intra>
2014-12-09 11:01:23 -0700

    working ajax calls for GET, PUT, POST

--- UserProjectRestClient/target/spring-mvc-webapp/js/User.js
+++ UserProjectRestClient/target/spring-mvc-webapp/js/User.js
@@ -1,24 +1,66 @@
-$(document).ready(function(){
+$(document).ready(function () {
     loadContacts();
-    
+
+    //on click for edit button
+    $('#editButton').click(function () {
+        $.ajax({
+            type: 'PUT',
+            url: 'http://localhost:8080/UserProjectHibernate/rest/user/' + $('#edit-id').val(),
+            data: JSON.stringify({
+                username: $('#edit-username').val(),
+                password: $('#edit-password').val()
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            'dataType': 'json'
+        }).success(function (data, status) {
+            $('#edit-username').val('');
+            $('#edit-password').val('');
+            loadContacts();
+        });
+    });
+
+    //code for add button
+    $('#addButton').click(function () {
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8080/UserProjectHibernate/rest/user',
+            data: JSON.stringify({
+                username: $('#add-username').val(),
+                password: $('#add-password').val()
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            'dataType': 'json'
+        }).success(function (data, status) {
+            username: $('#add-username').val('');
+            password: $('#add-password').val('');
+            loadContacts();
+        });
+    });
 });
 
-function loadContacts(){
+function loadContacts() {
     $('#user-list').empty();
     $.ajax({
-        url:"http://localhost:8080/UserProjectHibernate/rest/users"
-    }).then(function(data, status){
-        $.each(data, function(index, user){
-            $('#user-list').append('<tr><td>'+ user.id +'</td>' +'<td>' + user.username+'</td>' + '<td>' + user.password + '</td>' + '<td><a onclick="getEditUser(this);" href="#" id="'+user.id+'">Edit</a></td>' + '<td><a onclick="deleteUser(this);" href="#" id="'+user.id+'">Delete</a></td>'+'</tr>');
+        url: "http://localhost:8080/UserProjectHibernate/rest/users"
+    }).then(function (data, status) {
+        $.each(data, function (index, user) {
+            $('#user-list').append('<tr><td>' + user.id + '</td>' + '<td>' + user.username + '</td>' + '<td>' + user.password + '</td>' + '<td><a onclick="getEditUser(this);" href="#" id="' + user.id + '">Edit</a></td>' + '<td><a onclick="deleteUser(this);" href="#" id="' + user.id + '">Delete</a></td>' + '</tr>');
         });
     });
 }
 
-function getEditUser(element){
+function getEditUser(element) {
     $.ajax({
         url: 'http://localhost:8080/UserProjectHibernate/rest/user/' + element.id
-    }).success(function(data){
+    }).success(function (data) {
         $('#edit-username').val(data.username);
         $('#edit-password').val(data.password);
+        $('#edit-id').val(data.id);
     });
 }
